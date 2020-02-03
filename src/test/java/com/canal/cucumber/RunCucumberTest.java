2528790dd28949a4ee21073d11d583ffff23b71b
@MajdiZouari <majdi.zouari@outlook.com>
2020-02-03 15:37:00 +0100

    add cucumber to springboot context

--- src/test/java/com/canal/cucumber/RunCucumberTest.java
+++
@@ -1,9 +0,0 @@
-package com.canal.cucumber;
-import io.cucumber.junit.CucumberOptions;
-import net.serenitybdd.junit.runners.SerenityRunner;
-import org.junit.runner.RunWith;
-
-@RunWith(SerenityRunner.class)
-@CucumberOptions( features = "src/test/resources/cucumber",plugin = {"pretty", "json:target/cucumber-report.json"})
-public class RunCucumberTest {
-}
---
+++ src/test/java/com/canal/cucumber/SpringBootBaseIntegrationTest.java
@@ -0,0 +1,14 @@
+package com.canal.cucumber;
+
+import org.junit.platform.commons.logging.Logger;
+import org.junit.platform.commons.logging.LoggerFactory;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public abstract class SpringBootBaseIntegrationTest {
+    private static final Logger log = LoggerFactory.getLogger(SpringBootBaseIntegrationTest.class);
+}
